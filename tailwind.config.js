0754970d5c2997fe2fa50238e3f815856f58196a
Dylan <dalexander_02@arcadia.edu>
2024-04-11 18:14:53 -0400

    NAV AND ANIMATION WORKING WOOOO

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,25 @@ module.exports = {
     extend: {
       backgroundImage: {
         'sliding-pattern': "url('../background.jpg')"
+      },
+      keyframes: {
+        slide: {
+          '0%': {transform: 'translate3d(-1200px,0,0)', overflow: 'hidden'},
+          '100%': {transform: 'translate3d(0px, 0,0)', overflow: 'hidden'}
+        },
+        fade: {
+          '0%': {opacity:0},
+          '100%': {opacity: 1}
+        },
+        fadeBig: {
+          '0%': {fontSize: '16px'},
+          '100%': {fontSize: '19px'}
+        }
+      },
+      animation: {
+        slide: 'slide 60s linear infinite',
+        fade: 'fade 0.3s ease-in-out',
+        fadeBig: 'fadeBig 0.1s ease-in-out forwards'
       }
     },
   },
